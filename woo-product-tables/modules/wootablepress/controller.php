41b253baa9bcc6d39cf3cfdb99d0ac626143b80e
Sean Murphy <sean@iamseanmurphy.com>
2026-04-14 22:27:50 +0000

    2.2.9

--- woo-product-tables/modules/wootablepress/controller.php
+++ woo-product-tables/modules/wootablepress/controller.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * Product Table by WBW - WootablepressControllerWtbp Class
+ *
+ * @version 2.2.9
+ *
+ * @author  woobewoo
+ */
+
+defined( 'ABSPATH' ) || exit;
 
 class WootablepressControllerWtbp extends ControllerWtbp {
 
@@ -24,6 +33,20 @@ class WootablepressControllerWtbp extends ControllerWtbp {
 		}
 
 		return $data;
+	}
+
+	/**
+	 * getPermissions.
+	 *
+	 * @version 2.2.9
+	 * @since   2.2.9
+	 */
+	public function getPermissions() {
+		return array(
+			WTBP_USERLEVELS => array(
+				WTBP_ADMIN => array( 'delete' ),
+			),
+		);
 	}
 
 	public function getSearchProducts() {
